ea513214b9a05e3f2c88a42862f44465e7c4a01f
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-08-28 01:26:09 +0200

    Update dataFetcher.js

--- dataFetcher.js
+++ dataFetcher.js
@@ -6,8 +6,8 @@ import { Parser as Json2CsvParser } from 'json2csv';
 import { log } from './logger.js';
 
 const BINANCE_PAIR = 'BTCUSDT';
-const INTERVAL = '1h';
-const START_DATE = '2022-01-01T00:00:00Z';
+const INTERVAL = '3m';
+const START_DATE = '2025-07-01T00:00:00Z';
 const BATCH_SIZE = 1000;
 
 async function fetchBinanceOHLC(symbol, interval, startTime, limit) {
