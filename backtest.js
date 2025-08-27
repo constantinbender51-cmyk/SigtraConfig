319dd6d71f5e099895278822cb1f06b93bc84cb3
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-08-28 01:30:27 +0200

    Update backtest.js

--- backtest.js
+++ backtest.js
@@ -7,7 +7,7 @@ import { log } from './logger.js';
 
 // --- Configuration ---
 const config = {
-    DATA_FILE_PATH: './data/XBTUSD_60m_data.csv',
+    DATA_FILE_PATH: './data/XBTUSD_3m_data.csv',
     INITIAL_BALANCE: 10000,
     MINIMUM_CONFIDENCE_THRESHOLD: 0,
     MIN_SECONDS_BETWEEN_CALLS: 10,
