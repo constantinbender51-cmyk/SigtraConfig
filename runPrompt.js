2300565baca92616196a1981f34e51a8ed394635
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-08-30 13:08:02 +0200

    Update runPrompt.js

--- runPrompt.js
+++ runPrompt.js
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from "@google/generative-ai";
 
 async function runPrompt() {
   const ai = new GoogleGenAI({}); // Automatically reads GEMINI_API_KEY env var
